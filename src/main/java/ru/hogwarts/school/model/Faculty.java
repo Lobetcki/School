e694da559c2f634ea4db9b76f8ccc99b55e29ae8
Anton Mikhailouski <Lobetcki@gmail.com>
2023-03-09 18:26:31 +0300

    School,  school continuation 08.03.2023 исправлено

--- src/main/java/ru/hogwarts/school/model/Faculty.java
+++ src/main/java/ru/hogwarts/school/model/Faculty.java
@@ -27,49 +27,5 @@ public class Faculty {
         this.color = color;
     }
 
-    public Long getIdFaculty() {
-        return facultyId;
-    }
 
-    public void setIdFaculty(Long idFaculty) {
-        this.facultyId = idFaculty;
-    }
-
-    public String getNameFaculty() {
-        return nameFaculty;
-    }
-
-    public void setNameFaculty(String nameFaculty) {
-        this.nameFaculty = nameFaculty;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
-    }
-
-    @Override
-    public String toString() {
-        return "Facultu{" +
-                "idFaculty=" + facultyId +
-                ", nameFaculty='" + nameFaculty + '\'' +
-                ", color=" + color +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Faculty facultu = (Faculty) o;
-        return Objects.equals(facultyId, facultu.facultyId) && Objects.equals(nameFaculty, facultu.nameFaculty) && Objects.equals(color, facultu.color);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(facultyId, nameFaculty, color);
-    }
 }
--- src/main/java/ru/hogwarts/school/model/Student.java
+++ src/main/java/ru/hogwarts/school/model/Student.java
@@ -26,49 +26,4 @@ public class Student {
         this.ageStudent = ageStudent;
     }
 
-    public Long getIdStudent() {
-        return idStudent;
-    }
-
-    public void setIdStudent(Long id) {
-        this.idStudent = id;
-    }
-
-    public String getNameStudent() {
-        return nameStudent;
-    }
-
-    public void setNameStudent(String name) {
-        this.nameStudent = name;
-    }
-
-    public Integer getAgeStudent() {
-        return ageStudent;
-    }
-
-    public void setAgeStudent(Integer age) {
-        this.ageStudent = age;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Student student = (Student) o;
-        return Objects.equals(idStudent, student.idStudent) && Objects.equals(nameStudent, student.nameStudent) && Objects.equals(ageStudent, student.ageStudent);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(idStudent, nameStudent, ageStudent);
-    }
-
-    @Override
-    public String toString() {
-        return "Student{" +
-                "idStudent=" + idStudent +
-                ", nameStudent='" + nameStudent + '\'' +
-                ", ageStudent=" + ageStudent +
-                '}';
-    }
 }
